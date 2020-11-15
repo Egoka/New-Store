83894cd62661933820155f09b316c4ec063174c1
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-11-15 18:22:33 +0300

    Added product filter settings block
    + Added html markup in the filter.hbs file, with handlebars elements.
    + Added styles for elements in the styles.css file.

--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,11 @@
 M.AutoInit();
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
+});
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+    accordion: false
 });
 var instance = M.Carousel.init({
     fullWidth: true,
