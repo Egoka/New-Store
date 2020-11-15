ca637190755c859f6be8aec0b228dfadfb39d0a5
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-11-15 21:41:19 +0300

    Added tracking of a click on a filter field

--- static/scripts.js
+++ static/scripts.js
@@ -11,3 +11,14 @@ var instance = M.Carousel.init({
     fullWidth: true,
     indicators: true
 })
+const $filter = document.querySelector('.request')
+if ($filter){
+    $filter.addEventListener('click',event=>{
+        if (event.target.classList.contains('js-filter')){
+            const id = event.target.dataset.id
+            const state = event.srcElement.checked
+            const csrf = event.target.dataset.csrf
+            console.log(state,id)
+        }
+    })
+}
