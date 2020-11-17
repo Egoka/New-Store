0b669f4c9421b15dd69dc0431d6a44b2b46f6427
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-11-17 22:59:03 +0300

    Filter result product, query processing
    + Added scripts for viewing images in the scripts.js file for the price data format.
    + Also added a request for data parameters for adding a product to the list of favorite products, or adding it to the list of comparisons.

--- static/scripts.js
+++ static/scripts.js
@@ -3,7 +3,30 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+});
+document.querySelectorAll('.price').forEach(node =>{
+    node.textContent = new Intl.NumberFormat('ru-RU',{
+        currency:'rub',
+        style:'currency'
+    }).format(node.textContent)
+})
 var instance = M.Carousel.init({
     fullWidth: true,
     indicators: true
 })
+const $filter = document.querySelector('.request')
+if ($filter){
+    $filter.addEventListener('click',event=>{
+        if (event.target.classList.contains('js-filter')){
+            const id = event.target.dataset.id
+            const form = event.target.dataset.form
+            const status = event.target.dataset.status
+            const state = event.srcElement.checked
+            const csrf = event.target.dataset.csrf
+            console.log(id, form, status)
+        }
+    })
+}
