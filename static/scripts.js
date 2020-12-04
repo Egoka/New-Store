678c06380629e77276c731892dd809995cfb302c
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-12-04 21:08:22 +0300

    Fixed bugs added sealy
    + Changes in the styles.css file
    -- Removed scrollbar from entire site
    ++ Added styles that prohibit the selection of some elements.
    ++ The photoProduct class has been replaced with photoRecall so that there is no overlapping names.
    + Changes in hbs-helpers.js file
    ++ Created the starRecall() method for generating a message
    ++ Changed the stars() method, added parameters for defining an object in the output message.
    ++ Added toDate() method from scripts.js file where it was removed. It will now run on the backend.
    + Changes to the scripts.js file
    -- Removed toDate() method.
    ++ A method for listening to objects with the "recall-filter" class has been created
    + Changes to filteredProduct.hbs file
    ++ Changed addressing of links
    ++ Changed the form of displaying the list of parameters(through an array loop).
    + Changes in the file recall.hbs
    ++ Changed the format of the output of stars, to output through an array
    ++ The inference method of the "toDate" method is applied
    + Changes in shortProductDescription.hbs file
    ++ Changed format of data output through an array
    + Changed link path in carousel.hbs file.

--- static/scripts.js
+++ static/scripts.js
@@ -20,73 +20,6 @@ document.querySelectorAll('.price').forEach(node =>{
             style:'currency'
         }).format(node.textContent)}
 })
-const toDate = date => {
-    function monthDays(year, month) {
-        return (month == 2 ?
-            ((year % 4 != 0 ||
-                (year % 100 == 0 && year % 400 != 0)) ? 28 : 29) :
-            (((month < 8 && (month & 1) == 0) ||
-                (month > 7 && (month & 1) == 1)) ? 31 : 30));
-    }
-    function dateDiff(date1, date2) {
-        var years, months, days, hours, minutes, seconds;
-        var y1, m1, d1, d2, dd;
-        years = date2.getUTCFullYear()-(y1 = date1.getUTCFullYear());
-        months = date2.getUTCMonth()-(m1 = date1.getUTCMonth());
-        days = (d2 = date2.getUTCDate())-(d1 = date1.getUTCDate());
-        hours = date2.getUTCHours()-date1.getUTCHours();
-        minutes = date2.getUTCMinutes()-date1.getUTCMinutes();
-        seconds = date2.getUTCSeconds()-date1.getUTCSeconds();
-        dd = 0;
-        if (seconds < 0) {
-            seconds += 60;
-            minutes--;
-        }
-        if (minutes < 0) {
-            minutes += 60;
-            hours--;
-        }
-        if (hours < 0) {
-            hours += 24;
-            days--;
-            dd = 1;
-        }
-        if (days < 0) {
-            days = monthDays(y1, m1)-d1+d2-dd;
-            months--;
-        }
-        if (months < 0) {
-            months += 12;
-            years--;
-        }
-        if (years>0)return `${years} год назад`
-        if (months>0) {
-            if(months===1)return `${months} месяц назад`
-            if(months<5)return `${months} месяца назад`
-            return `${months} месяцев назад`
-        }
-        if (days>0) {
-            if(days===1)return `${days} час назад`
-            if(days<5)return `${days} часа назад`
-            return `${days} дня назад`
-        }
-        if (hours>0){
-            if(hours===1)return `${hours} час назад`
-            if(hours<5)return `${hours} часа назад`
-            return `${hours} часов назад`}
-        if (minutes>0) {
-            if(minutes===1)return `${minutes} минуту назад`
-            if(minutes<5)return `${minutes} минуты назад`
-            return `${minutes} минут назад`}
-        if (seconds>0)return `${seconds} секунд назад`
-    }
-    var d1 = new Date(date);
-    var d2 = new Date(+Date.now() * 1)
-    return dateDiff(d1, d2);
-}
-document.querySelectorAll('.date').forEach(node => {
-    node.textContent = toDate(node.textContent)
-})
 var instance = M.Carousel.init({
     fullWidth: true,
     indicators: true
@@ -102,6 +35,15 @@ if ($filter){
             const status = event.target.dataset.status
             console.log(id, form, status)
             const csrf = event.target.dataset.csrf
+        }
+    })
+}
+const $filterRecall = document.querySelector('.filterRecall')
+if ($filterRecall){
+    $filterRecall.addEventListener('click',event=>{
+        if (event.target.classList.contains('recall-filter')){
+            const star = event.target.dataset.id
+            console.log(star)
         }
     })
 }
--- utils/hbs-helpers.js
+++ utils/hbs-helpers.js
@@ -1,15 +1,23 @@
 module.exports = {
     stars(rating){
+        const size = rating
         rating=rating.toFixed(0)
         const star = new Array(5).fill("#bcbcbc");
         while(rating) {
             rating -= 1
             star[rating] = "#e7ad00"}
-        return (`<i class="material-icons" style="color: ${star[0]}">star</i>
-                 <i class="material-icons" style="color: ${star[1]}">star</i>
-                 <i class="material-icons" style="color: ${star[2]}">star</i>
-                 <i class="material-icons" style="color: ${star[3]}">star</i>
-                 <i class="material-icons" style="color: ${star[4]}">star</i>`)
+        return (`<i data-id="${size}" class="material-icons recall-filter" style="color: ${star[0]}">star</i>
+                 <i data-id="${size}" class="material-icons recall-filter" style="color: ${star[1]}">star</i>
+                 <i data-id="${size}" class="material-icons recall-filter" style="color: ${star[2]}">star</i>
+                 <i data-id="${size}" class="material-icons recall-filter" style="color: ${star[3]}">star</i>
+                 <i data-id="${size}" class="material-icons recall-filter" style="color: ${star[4]}">star</i>`)
+    },
+    starRecall(number){
+        if (number>0) {
+            if (number === 1) return `${number} отзыв`
+            if (number < 5) return `${number} отзыва`
+            return `${number} отзывов`}
+        return `--`
     },
     paymentFormat(format){
         if(format==='courier'){
@@ -20,5 +28,63 @@ module.exports = {
             return`<i class="material-icons">store</i>
                    <p>Варианты оплаты уточняйте в магазине</p>`
         }
+    },
+    toDate(date){
+        function monthDays(year, month) {
+            return (month === 2 ?
+                ((year % 4 !== 0 ||
+                    (year % 100 === 0 && year % 400 !== 0)) ? 28 : 29) :
+                (((month < 8 && (month & 1) === 0) ||
+                    (month > 7 && (month & 1) === 1)) ? 31 : 30));}
+        function dateDiff(date1, date2) {
+            var years, months, days, hours, minutes, seconds;
+            var y1, m1, d1, d2, dd;
+            years = date2.getUTCFullYear()-(y1 = date1.getUTCFullYear());
+            months = date2.getUTCMonth()-(m1 = date1.getUTCMonth());
+            days = (d2 = date2.getUTCDate())-(d1 = date1.getUTCDate());
+            hours = date2.getUTCHours()-date1.getUTCHours();
+            minutes = date2.getUTCMinutes()-date1.getUTCMinutes();
+            seconds = date2.getUTCSeconds()-date1.getUTCSeconds();
+            dd = 0;
+            if (seconds < 0) {
+                seconds += 60;
+                minutes--;}
+            if (minutes < 0) {
+                minutes += 60;
+                hours--;}
+            if (hours < 0) {
+                hours += 24;
+                days--;
+                dd = 1;}
+            if (days < 0) {
+                days = monthDays(y1, m1)-d1+d2-dd;
+                months--;}
+            if (months < 0) {
+                months += 12;
+                years--;}
+            if (years>0) {
+                if (months === 1) return `${months} год назад`
+                if (months < 5) return `${months} года назад`
+                return `${years} лет назад`}
+            if (months>0) {
+                if(months===1)return `${months} месяц назад`
+                if(months<5)return `${months} месяца назад`
+                return `${months} месяцев назад`}
+            if (days>0) {
+                if(days===1)return `${days} день назад`
+                if(days<5)return `${days} дня назад`
+                return `${days} дней назад`}
+            if (hours>0){
+                if(hours===1)return `${hours} час назад`
+                if(hours<5)return `${hours} часа назад`
+                return `${hours} часов назад`}
+            if (minutes>0) {
+                if(minutes===1)return `${minutes} минуту назад`
+                if(minutes<5)return `${minutes} минуты назад`
+                return `${minutes} минут назад`}
+            if (seconds>0)return `${seconds} секунд назад`}
+        var d1 = new Date(date);
+        var d2 = new Date(+Date.now())
+        return dateDiff(d1, d2);
     }
 }
