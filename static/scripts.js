a5b3a7ae3aac0d29925e83b195dfa5d57b4c5cc7
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-01-31 22:13:41 +0300

    Added new functionality to the Carousel block.
    + Added functional slide switch buttons for the carousel block
    + The slider method has been rewritten for the function buttons to work in the scripts.js file. Inside the created object, functions for switching buttons were declared.
    + Added css styles for toggle buttons in the styles.css file.

--- static/scripts.js
+++ static/scripts.js
@@ -23,10 +23,6 @@ document.querySelectorAll('.price').forEach(node =>{
 $(document).ready(function() {
     $('input#input_text, textarea#textarea2').characterCounter();
 });
-var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true
-})
 const $filter = document.querySelector('.request')
 if ($filter){
     $filter.addEventListener('click',event=>{
@@ -80,3 +76,11 @@ if ($typeProducts){
         }
     })
 }
+$(document).ready(function () {
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.slide-prev').click(function(){
+        $('.carousel').carousel('prev')});
+    $('.slide-next').click(function(){
+        $('.carousel').carousel('next')});
+});
