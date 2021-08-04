e7eab004c8cbe98b291a47f982382541752b1e78
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-08-04 15:52:26 +0300

    Made Router processing for editing an account
    An overwrite of the user's personal data has been created. And also saving the color theme of the account.

--- crs/Account/account.js
+++ crs/Account/account.js
@@ -1,12 +1,25 @@
 const {Router} = require('express')
+const mongoose = require('mongoose')
 const router = Router()
-const {account, orders} = require('../inf/filter.js')
-router.get('/',(req, res) => {
+const Users = require('../../modelsDB/users')
+router.get('/',async (req, res) => {
+    const account = await Users.findById(req.session.user._id,
+        'photoUrl fullName email theme verifiedUser').lean()
     res.render('account', {
         title: 'Профиль',
         privateOffice:true,
         account,
-        orders
     })
+})
+router.post('/editAccout', async (req,res)=>{
+    const {photoUrl, fullName} = req.body
+    await Users.findByIdAndUpdate(req.session.user._id, {photoUrl, fullName})
+    Object.assign(req.session.user, req.body)
+    req.session.save()
+})
+router.post('/editTheme', async (req,res)=>{
+    await Users.findByIdAndUpdate(req.session.user._id, req.body)
+    Object.assign(req.session.user, req.body)
+    req.session.save()
 })
 module.exports = router
