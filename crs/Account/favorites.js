cc8a8418858eee582eeae81e3ac6d5b768e20dd8
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-08-05 11:02:02 +0300

    Added loading of favorite products
    Implemented loading of products of the same type that the user liked.

--- crs/Account/favorites.js
+++ crs/Account/favorites.js
@@ -1,13 +1,33 @@
 const {Router} = require('express')
 const router = Router()
-const {products, productTypes, account} = require('../inf/filter.js')
-router.get('/',(req, res) => {
+const Users = require('../../modelsDB/users')
+const mongoose = require("mongoose");
+router.get('/:id',async (req, res) => {
+    let products = await Users.aggregate([
+        { $match: { _id: mongoose.Types.ObjectId(req.session.user._id) } },
+        { $group: { _id: "$favorites._id"}},
+        { $unwind: "$_id"},
+        { $lookup: {
+                from: "products",
+                let: { id: "$_id" },
+                pipeline: [
+                    { $match: { $expr: { $eq: ["$_id", "$$id"] }}},
+                    { $project:{_id:1, nameProduct:1, photoURL:1, price:1, depiction:1,classProduct:1}}
+                ], as: "_id"}},
+        { $match: { "_id.classProduct": mongoose.Types.ObjectId(inferredClass) } },
+        { $project:{
+                _id: { $first: "$_id._id" },
+                nameProduct: { $first: "$_id.nameProduct" },
+                photoURL: { $first: "$_id.photoURL" },
+                price:{ $first: "$_id.price" },
+                depiction:{ $first: "$_id.depiction" },
+            }}
+    ])
     res.render('favorites', {
         title: 'Избранное',
         favorites: true,
-        account,
         products,
-        options:productTypes
+        options
     })
 })
 module.exports = router
