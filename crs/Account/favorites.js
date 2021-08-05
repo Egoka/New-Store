4de54bfc7e55861e4edbe82164e4fe070c50caeb
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-08-05 11:32:53 +0300

    Select to load product types for the favored products page

--- crs/Account/favorites.js
+++ crs/Account/favorites.js
@@ -3,6 +3,30 @@ const router = Router()
 const Users = require('../../modelsDB/users')
 const mongoose = require("mongoose");
 router.get('/:id',async (req, res) => {
+    let options = await Users.aggregate([
+        { $match: { _id : mongoose.Types.ObjectId(req.session.user._id)} },
+        { $group: { _id: "$favorites._id"}},
+        { $unwind: "$_id"},
+        { $lookup: {
+                from: "products",
+                let: { id: "$_id" },
+                pipeline: [
+                    { $match: { $expr: { $eq: ["$_id", "$$id"] }}},
+                    { $project:{classProduct:1}}
+                ], as: "id"}},
+        { $group: { _id: "$id.classProduct"}},
+        { $lookup: {from: "classproducts", localField: "_id", foreignField: "_id", as: "_id"}},
+        { $sort : {"_id.priority":1 } },
+        { $project:{
+                _id: { $first: "$_id._id" },
+                className:{ $first: "$_id.className" },
+                classIco:{ $first: "$_id.classIco" },
+            }} ])
+    let inferredClass
+    if(options.length>0){
+        if(req.params.id==='default') {
+            inferredClass = options[0]._id.toString()
+        }else{inferredClass = req.params.id}}
     let products = await Users.aggregate([
         { $match: { _id: mongoose.Types.ObjectId(req.session.user._id) } },
         { $group: { _id: "$favorites._id"}},
