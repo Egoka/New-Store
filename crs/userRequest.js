ea7f5796746baf8a906619022ce613107de0976f
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-08-05 17:36:03 +0300

    Methods for editing the basket have been developed
    * In /static/fetchRequests.js file created two functions with fetch method. The userBasket() method implements adding a product to the cart or removing a product from the cart. The userEditCountProduct() method is intended to change the quantity of a product in the cart, however it can also be used to remove a product from the cart.
    * A router is created in the /crs/userRequest.js file to add or remove a product from the cart. The method, getting the id of the product, first checks whether the user already has a product in the cart. if the product is already there, then it is removed from the basket. If it is not there, then it is added to the cart.
    Another method changes the value of 'count' to the value it received.

--- crs/userRequest.js
+++ crs/userRequest.js
@@ -28,4 +28,40 @@ router.get('/list/:id',async (req, res) => {
         res.json(true)
     }
 })
+router.get('/replenishBasket/:id/:ids',async (req, res) => {
+    const user = await Users.findById(req.session.user._id,
+        {basket:{$elemMatch:{productId: req.params.id,idSeller: req.params.ids}}}).lean()
+    if(user.basket){
+        await Users.findByIdAndUpdate(req.session.user._id,
+            {$pull:{basket:{productId:req.params.id,idSeller: req.params.ids}}}).then(res.json(false))
+    }else{
+        await Users.findByIdAndUpdate(req.session.user._id,
+            {$push:{basket:{productId:req.params.id,idSeller: req.params.ids}}}).then(res.json(true))}
+})
+router.get('/basket/:id/:direction',async (req, res) => {
+    if(req.params.direction==0){
+        await Users.findByIdAndUpdate(req.session.user._id,
+            {$pull:{basket:{_id:req.params.id}}})
+            .lean().exec( async (err,user)=>{
+                if(err){throw err}
+                res.json({count:0,direction:-1})})}
+    if(req.params.direction==1){
+        await Users.findByIdAndUpdate(req.session.user._id,
+            { $inc:{'basket.$[element].count':1}},
+            { arrayFilters:[{'element._id':{$eq:req.params.id}}]})
+            .lean().exec( async (err,user)=>{
+                if(err){throw err}
+                res.send({count:user.basket.find(product => product._id.toString() === req.params.id).count+1,direction:1})})}
+    if(req.params.direction==-1){
+        await Users.findByIdAndUpdate(req.session.user._id,
+            { $inc:{'basket.$[element].count':-1}},
+            { arrayFilters:[{'element._id':{$eq:req.params.id}}]})
+            .lean().exec( async (err,user)=>{
+                if(err){throw err}
+                const count = user.basket.find(product=> product._id.toString() === req.params.id).count-1
+                if(count===0){
+                    await Users.findByIdAndUpdate(req.session.user._id,
+                        {$pull:{basket:{_id:req.params.id}}})}
+                res.json({count,direction:-1})})}
+})
 module.exports = router
--- static/fetchRequests.js
+++ static/fetchRequests.js
@@ -49,3 +49,40 @@ function userListDel(event, id) {
                     window.location.reload()}
                     },1000)
         })}
+function userBasket(event, id) {
+    const ids = event.target.parentElement.dataset.busketid
+    fetch('/request/replenishBasket/' + id+'/'+ids,{method: 'get'})
+        .then(res => res.json())
+        .then(callback =>{
+            if(callback){
+                document.querySelectorAll(`.btnProduct[data-busketid='${ids}']`).forEach(button=>{
+                    button.children[0].innerText = "shopping_cart"
+                    button.children[1].innerText = "в корзине"})
+            }else{
+                document.querySelectorAll(`.btnProduct[data-busketid='${ids}']`).forEach(button=>{
+                    button.children[0].innerText = "add_shopping_cart"
+                    button.children[1].innerText = "в корзину"})
+            }
+        })}
+function userEditCountProduct(event, id, mode) {
+    fetch('/request/basket/' + id+'/'+mode,{method: 'get'})
+        .then(res => res.json())
+        .then(callback =>{
+            const basketPrice = document.querySelector('.basketPrice')
+            basketPrice.children[0].children[0].innerHTML=Number(basketPrice.children[0].children[0].innerHTML)+callback.direction
+            let sum=+document.querySelector('#sum').innerHTML
+                .split(',')[0].split("&nbsp;").join('')
+            let price = +event.composedPath()[3].children[0].children[0].innerHTML
+                .split(',')[0].split("&nbsp;").join('')
+            document.querySelector('#sum').innerHTML = new Intl.NumberFormat('ru-RU',{
+                currency:'rub', style:'currency'
+            }).format(sum+price*callback.direction)
+            if(callback.count===0){
+                if(event.composedPath()[4].children[1].children.length==1){
+                    event.target.parentElement.parentElement.parentElement.parentElement.remove()
+                }else{event.target.parentElement.parentElement.remove()}}
+            if(callback.count>0){
+                event.target.parentElement.children[1].innerHTML=callback.count}
+            if(document.querySelector('.bodyBusket').children.length==0){
+                window.location.reload()}
+        })}
