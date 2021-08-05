a3207e6ec7ae27595c56f92fa5611e3ef3ed09ae
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-08-05 15:13:41 +0300

    A method for getting all products from the basket
    The method implements a request to the list of models stored in the basket. After receiving the list of models, through the cycle, the quantity and the sum of the cost of the products are calculated.

--- crs/Account/basket.js
+++ crs/Account/basket.js
@@ -2,6 +2,51 @@ const {Router} = require('express')
 const mongoose = require('mongoose')
 const router = Router()
 const Users = require('../../modelsDB/users')
+async function getAllProductsFromBusket(userID) {
+    const products = await Users.aggregate([
+        { "$match": { "_id": mongoose.Types.ObjectId(userID) } },
+        {"$project": {"basket": 1,"_id":0}},
+        { "$lookup": {
+                from: "products",
+                let: { productId: "$basket.productId", isSeller:"$basket.idSeller" },
+                pipeline: [
+                    { "$match": { $expr: { $in: [ "$_id",  "$$productId"] }}},
+                    { "$project": {
+                            "nameProduct": 1,
+                            "listSeller": {
+                                "$filter": {input: "$listSeller", as: "item",
+                                    cond: { $in: [ "$$item._id", "$$isSeller"] }}},
+                            "_id": 1 } },
+                    { "$lookup": {
+                            from: "sellers",
+                            let: { idSeller: "$listSeller.idSeller" },
+                            pipeline: [
+                                { "$match": { $expr: { $in: ["$_id", "$$idSeller"] }}},
+                                { "$project":{fullName:1,_id:1}}
+                            ], as: "idSeller"}},
+                ], as: "products" }},
+        { "$project": {
+                'basket': {_id:1,count:1, idSeller:1},
+                'products':{_id:1,nameProduct:1,
+                    listSeller:{_id:1, idSeller:1, price:1},
+                    idSeller:1}
+            }}])
+    let sumPrise=0;let sizeBasket=0
+    const basket = products[0].basket;const basketList = products[0].products
+    basketList.forEach(object=>{
+        const listNameSellers = object.idSeller
+        object.listSeller.forEach(product=> {
+            const productInBasket = basket.find(seller =>
+                seller.idSeller.toString() === product._id.toString())
+            product._id = productInBasket._id.toString()
+            product.count = productInBasket.count
+            product.nameSeller = listNameSellers.find(nameSeller =>
+                nameSeller._id.toString()===product.idSeller.toString()).fullName
+            sumPrise+=product.price*product.count
+            sizeBasket+=product.count
+            delete product.idSeller})
+        delete object.idSeller})
+    return {basketList,sumPrise,sizeBasket}}
 router.get('/',async (req, res) => {
     const{basketList, sumPrise, sizeBasket} = await getAllProductsFromBusket(req.session.user._id)
     res.render('basket', {
