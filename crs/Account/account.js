1403fa68b3fa396f62d46356dc4a6335bd397cf9
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-08-05 15:40:53 +0300

    Implemented loading of all user orders
    Orders sorted by date descending.

--- crs/Account/account.js
+++ crs/Account/account.js
@@ -5,10 +5,16 @@ const Users = require('../../modelsDB/users')
 router.get('/',async (req, res) => {
     const account = await Users.findById(req.session.user._id,
         'photoUrl fullName email theme verifiedUser').lean()
+    const orders = await Users.aggregate([
+        { $match: { "_id": mongoose.Types.ObjectId(req.session.user._id) } },
+        { $unwind: '$orders' },
+        { $sort: { 'orders.date': -1 }},
+        { $group: { _id: '$_id', orders: { $push: '$orders'}}}])
     res.render('account', {
         title: 'Профиль',
         privateOffice:true,
         account,
+        orders:orders[0]?orders[0].orders:Array()
     })
 })
 router.post('/editAccout', async (req,res)=>{
