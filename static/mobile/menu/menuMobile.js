ddc35496f50e4dea85039e533acb6a245a3f9eed
Egor Bondarenko <eg.bonda2014@gmail.com>
2021-09-02 16:23:50 +0300

    Initial conditions of the interface
    Created a basic fulfillment condition only for Android and iPhone devices. On the mobile version, the menu interface starts with a parent category field, listCategory, which will fill the entire width of the screen. And the listType and listSearch blocks at the start position will be equal to zero.
    Also, all roundingArrow elements must be hidden in the listCategory block.
    Created a touchend event listener for backgroundCategory blocks from the global menu field.

--- static/mobile/menu/menuMobile.js
+++ static/mobile/menu/menuMobile.js
@@ -1,3 +1,15 @@
 let feieldType = document.querySelector('.listType')
 if(feieldType){feieldType.addEventListener('touchend',event=>{
     if (event.target.classList.contains('backgroundCategory')){event.preventDefault()} })}
+/*---*/
+if (/Android|iPhone/i.test(navigator.userAgent)) {
+    document.querySelector('.positionCategory.listType').style.maxWidth="0"
+    document.querySelector('.positionCategory.listSearch').style.maxWidth="0"
+    Array.from(document.querySelectorAll(
+        '.listCategory .roundingArrow[style]'
+    )).map(list=>{list.removeAttribute('style')})
+    let menu =document.querySelector('.menu')
+    if(menu){menu.addEventListener('touchend',event=>{
+        if (event.target.classList.contains('backgroundCategory')){
+        } })}
+}
